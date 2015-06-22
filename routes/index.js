f761102f78604f9433b04474be1f78f6fffc28ff
Franklin Ospino <ospi89@hotmail.com>
2015-06-21 19:16:32 -0500

    version 4 corregido el get por put

--- routes/index.js
+++ routes/index.js
@@ -44,7 +44,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 /* ----------------- Comentarios ---------------------- */
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired , commentController.publish);
+router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired , commentController.publish);
 
 
 module.exports = router;
